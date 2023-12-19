43a3704e0768b5c07e0c5dd32685d2a7f22a759e
yoshzawa <yoshzawa@users.noreply.github.com>
2023-12-19 13:10:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,16 +8,6 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/sayHello/{name}")
-async def dynamic_content(name: str):
-    template_path = Path(__file__).parent / "sayHello.html"
-    if not template_path.exists():
-        return PlainTextResponse("Error: File not found", status_code=500)
-    html_content = template_path.read_text()
-    dynamic_html = html_content.replace("{{ name }}", name)  
-    html_content = template_path.read_text()
-    return {"path": template_path}
-
-@app.get("/sayHello3/{name}")
 async def dynamic_content3(name: str):
     template_path = Path(__file__).parent / "sayHello.html"
     if not template_path.exists():
@@ -25,30 +15,3 @@ async def dynamic_content3(name: str):
     html_content = template_path.read_text()
     dynamic_html = html_content.replace("{{ name }}", name)  
     return Response(content=dynamic_html, media_type="text/html")
-
-@app.get("/sayHello2/{name}")
-async def dynamic_content2(name: str):
-    try:
-        template_path = Path(__file__).parent / "sayHello.html"
-    except Exception as e:
-        print(f"Error: {e}")
-        return PlainTextResponse(f"Error1: {e}", status_code=500)
-
-    if not template_path.exists():
-        return PlainTextResponse("Error: File not found", status_code=500)
-    
-    try:
-        html_content = template_path.read_text()
-    except Exception as e:
-        print(f"Error: {e}")
-        return PlainTextResponse(f"Error2: {e}", status_code=500)
-    try:
-        dynamic_html = html_content.replace("{{ name }}", name)  
-    except Exception as e:
-        print(f"Error: {e}")
-        return PlainTextResponse(f"Error3: {e}", status_code=500)
-    try:
-        return FileResponse(content=dynamic_html, media_type="text/html")
-    except Exception as e:
-        print(f"Error: {e}")
-        return PlainTextResponse(f"Error4: {e}", status_code=500)
