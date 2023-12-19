a9d0ff53dc34416474ff59281aafb5b53529cf39
yoshzawa <yoshzawa@users.noreply.github.com>
2023-12-19 11:37:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/sayHello/{name}")
+@app.get("/sayHello2/{name}")
 async def dynamic_content(name: str):
 
     if not template_path.exists():
@@ -16,16 +16,20 @@ async def dynamic_content(name: str):
     try:
         template_path = Path(__file__).parent / "sayHello.html"
     except Exception as e:
+        print(f"Error: {e}")
         return PlainTextResponse(f"Error1: {e}", status_code=500)
     try:
         html_content = template_path.read_text()
     except Exception as e:
+        print(f"Error: {e}")
         return PlainTextResponse(f"Error2: {e}", status_code=500)
     try:
         dynamic_html = html_content.replace("{{ name }}", name)  
     except Exception as e:
+        print(f"Error: {e}")
         return PlainTextResponse(f"Error3: {e}", status_code=500)
     try:
         return FileResponse(content=dynamic_html, media_type="text/html")
     except Exception as e:
+        print(f"Error: {e}")
         return PlainTextResponse(f"Error4: {e}", status_code=500)
