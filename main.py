fb99a92a1b7b2a9dee8eba474643d681d3baed64
yoshzawa <yoshzawa@users.noreply.github.com>
2023-12-19 10:08:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def read_root():
 @app.get("/sayHello/{name}")
 async def dynamic_content(name: str):
     try:
-      html_content = open("sayHello.html").read()  
-      dynamic_html = html_content.replace("{{ name }}", name)  
-      return FileResponse(content=dynamic_html, media_type="text/html")
+        template_path = Path(__file__).parent / "sayHello.html"
+        html_content = template_path.read_text()
+        dynamic_html = html_content.replace("{{ name }}", name)  
+        return FileResponse(content=dynamic_html, media_type="text/html")
     except Exception as e:
-      return PlainTextResponse(f"Error: {e}", status_code=500)
+        return PlainTextResponse(f"Error: {e}", status_code=500)
